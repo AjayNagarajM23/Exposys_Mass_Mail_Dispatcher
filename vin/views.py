680016ed89296c04cb5c2dc00c82ec811c92e9f7
AjayNagarajM23 <ajaynagarajm23@gmail.com>
2022-12-29 11:07:33 +0530

    First Commit

--- vin/views.py
+++ vin/views.py
@@ -1,5 +1,4 @@
 import string
-import time
 
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
@@ -9,7 +8,8 @@ from django.contrib import messages
 from django.conf import settings
 from django.core.mail import send_mail
 import requests
-
+import time
+import re
 
 
 # Create your views here.
@@ -43,17 +43,25 @@ def verifymail(request):
         for ele in arr:
             ele = ele + ""
             email_address = ele
-            response = requests.get(
-                "https://isitarealemail.com/api/email/validate",
-                params={'email': email_address})
 
-            status = response.json()['status']
-            if status == "valid":
-                continue
-            elif status == "invalid":
-                messages.warning(request, ele)
+            pat = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+            if re.match(pat, email_address):
+                response = requests.get(
+                    "https://isitarealemail.com/api/email/validate",
+                    params={'email': email_address})
+
+                status = response.json()['status']
+
+                if status == "valid":
+                    continue
+                elif status == "invalid":
+                    messages.warning(request, ele)
+                else:
+                    messages.warning(request, ele)
             else:
                 messages.warning(request, ele)
+
+            time.sleep(2)
         return render(request, 'infomail.html')
     return render(request, 'verifymail.html')
 
@@ -71,28 +79,33 @@ def sendmail(request):
     if request.method == 'POST':
         subject = request.POST['subject']
         content = request.POST['content']
+        # deltime = request.POST['delay']
         file = request.FILES["myFile"]
-        delTime = request.POST['delay']
         df = pd.read_csv(file)
         arr = df["mail"]
-        time.sleep(int(delTime)*60)
+        # time.sleep(int(deltime)*60)
+
         for ele in arr:
             ele = ele + ""
-            email_address = ele
-            response = requests.get(
-                "https://isitarealemail.com/api/email/validate",
-                params={'email': email_address})
-
-            status = response.json()['status']
-            if status == "valid":
-                email_from = settings.EMAIL_HOST_USER
-                recipient_list = [ele]
-                send_mail(subject, content, email_from, recipient_list)
-                messages.info(request, ele)
-            elif status == "invalid":
-                messages.warning(request, ele)
+            pat = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+            if re.match(pat, ele):
+                response = requests.get(
+                    "https://isitarealemail.com/api/email/validate",
+                    params={'email': ele})
+                status = response.json()['status']
+                if status == "valid":
+                    email_from = settings.EMAIL_HOST_USER
+                    recipient_list = [ele]
+                    send_mail(subject, content, email_from, recipient_list)
+                    messages.info(request, ele)
+                elif status == "invalid":
+                    messages.warning(request, ele)
+                else:
+                    messages.warning(request, ele)
             else:
                 messages.warning(request, ele)
+
+            time.sleep(2)
         return render(request, 'confirmation.html')
     return render(request, 'sendmail.html')
 
